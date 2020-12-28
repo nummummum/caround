ebf146e7dc0077bc63f64936cd68128a822cbdcc
skdn4879 <skanxkrl3645@naver.com>
2020-12-28 21:28:15 +0900

    다 바꾼듯?

--- app/src/main/java/com/projes/caround/MainActivity.java
+++ app/src/main/java/com/projes/caround/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment1 fragment1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        getSupportFragmentManager().beginTransaction().add(R.id.mainContainer, fragment1).commit();
+
     }
 }
